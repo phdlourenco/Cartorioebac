270631b1cf547fe011f7378a491943c7dca03400
phdlourenco <111437511+phdlourenco@users.noreply.github.com>
2024-06-13 21:28:04 -0300

    Opção de sair do programa
    Incluído no sistema a opção para o usuário de sair do programa

--- cartorio_pedro.c
+++ cartorio_pedro.c
@@ -124,8 +124,9 @@ int main()
 		printf("Escolha a opção desejada no menu: \n\n" );
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n\n"); //fim do menu
-		printf("Opção: ");
+		printf("\t3 - Deletar nomes\n\n\n"); 
+		printf("\t4 - Sair do sistema\n\n");
+		printf("Opção: ");//fim do menu
 	
 		scanf("%d", &opcao ); //armazenando a escolha do usuário
 	
@@ -143,6 +144,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
